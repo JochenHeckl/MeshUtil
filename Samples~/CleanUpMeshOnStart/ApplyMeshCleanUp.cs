7b17fa7a63cfda3a59a86d7df481165989bda05e
Jochen Heckl <jochen.heckl@gmail.com>
2020-05-29 11:38:37 +0200

    Fixed pending merge conflict

--- Samples~/CleanUpMeshOnStart/ApplyMeshCleanUp.cs
+++ Samples~/CleanUpMeshOnStart/ApplyMeshCleanUp.cs
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-﻿using UnityEngine;
+using UnityEngine;
 using de.JochenHeckl.Unity.MeshUtil;
-=======
-﻿using UnityEngine;
-using de.JochenHeckl.MeshUtil;
 using System.Linq;
->>>>>>> 13042a54980e9a6f612a67d9e4fb99f8af988b55
 
 public class ApplyMeshCleanUp : MonoBehaviour
 {
@@ -21,11 +16,9 @@ public class ApplyMeshCleanUp : MonoBehaviour
             cleanedMesh.vertices = weldResult.remappedVertices.ToArray();
             cleanedMesh.uv = baseMesh.uv.RemapUvs( weldResult.vertexRemaps );
 
-<<<<<<< HEAD
             Debug.Log( $"Mesh cleanup reduced vertex count from {baseMesh.vertexCount} to {meshFilter.mesh.vertexCount}." );
             Debug.Log( $"Mesh cleanup reduced triangle count from {baseMesh.triangles.Length/3} to {meshFilter.mesh.triangles.Length / 3}." );
-        }
-=======
+        
             cleanedMesh.subMeshCount = baseMesh.subMeshCount;
 
             foreach ( var subMeshIdx in Enumerable.Range( 0, baseMesh.subMeshCount ) )
@@ -42,6 +35,5 @@ public class ApplyMeshCleanUp : MonoBehaviour
             Debug.Log( $"Mesh cleanup reduced vertex count from {baseMesh.vertexCount} to {cleanedMesh.vertexCount}." );
             Debug.Log( $"Mesh cleanup reduced triangle count from {baseMesh.triangles.Length/3} to {cleanedMesh.triangles.Length / 3}." );
         }
->>>>>>> 13042a54980e9a6f612a67d9e4fb99f8af988b55
     }
 }
